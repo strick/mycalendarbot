64ce1241f513e84ef73080c29fd928aba446969b
Brian Strickland <bstrick@gmail.com>
2023-08-23 08:47:45 -0400

    Code in place for calendar, but 401 error

--- app.js
+++ app.js
@@ -72,13 +72,23 @@ function ensureScope (scope) {
     if (!msalRequest.scopes.some((s) => s.toLowerCase() === scope.toLowerCase())) {
         msalRequest.scopes.push(scope);
     }
-}
+} 
+
+app.get('/signout', (req, res) => {
+    // Clear the session data
+    req.session.destroy(() => {
+        // Redirect to Azure AD's logout endpoint
+        res.redirect(`https://login.microsoftonline.com/${process.env.DIRECTORY_ID}/oauth2/logout?post_logout_redirect_uri=${encodeURIComponent(process.env.REDIRECT_URI)}`);
+    });
+});
+
+
 
 const cca = new msal.ConfidentialClientApplication(msalConfig);
 
 app.get("/signIn", async (req, res) => {
     const authCodeUrlParameters = {
-        scopes: ["user.read"],
+        scopes: ["user.read", "Calendars.Read", "Calendars.ReadWrite", "Calendars.ReadBasic", "Calendars.Read.Shared"],
         redirectUri: process.env.REDIRECT_URI
     };
 
@@ -96,38 +106,72 @@ async function getToken(req) {
         throw new Error(
             'User info cleared from session. Please sign out and sign in again.');
     }
-    try {
+   try {
         // First, attempt to get the token silently
         const silentRequest = {
             scopes: msalRequest.scopes,
-            account: account.username//cca.getAccountByUsername(account)
+            account: account//cca.getAccountByUsername(account)
         };
 
         const silentResult = await cca.acquireTokenSilent(silentRequest);
+        console.log("Silent otken:");
+        console.log(silentResult.accessToken);
         return silentResult.accessToken;
     } catch (silentError) {
-        // If silent requests fails with InteractionRequiredAuthError,
-        // attempt to get the token interactively
-        if (silentError instanceof msal.InteractionRequiredAuthError) {
-            const interactiveResult = await msalClient.acquireTokenPopup(msalRequest);
-            return interactiveResult.accessToken;
-        } else {
             throw silentError;
-        }
+   
+        
     }
 }
+
+app.get('/events', async (req, res) => {
+
+    ensureScope("Calendars.Read", "Calendars.ReadWrite", "Calendars.ReadBasic", "Calendars.Read.Shared");
+
+    // After acquiring the token, use it to fetch user details:
+    // Initialize Graph client with a function as the authProvider
+    const client = MicrosoftGraph.Client.init({
+        authProvider: async (done) => {
+            try {
+                const token = await getToken(req);
+                done(null, token);  // Pass the token to the SDK
+            } catch (error) {
+                done(error, null); // Pass the error if there's one
+            }
+        }
+    });
+
+
+    const dateNow = new Date();
+    const dateNextWeek = new Date();
+    dateNextWeek.setDate(dateNextWeek.getDate() + 7);
+    const query = `startDateTime=${dateNow.toISOString()}&endDateTime=${dateNextWeek.toISOString()}`;
+
+    
+    try {
+        let events = await client
+            //.api('/me/calendarView').query(query)
+            .api('/me/events')
+            //.api('/me/findMeetingTimes')
+            //.select('subject,start,end')
+            //.orderby('start/DateTime')
+
+            .get();
+
+            console.log(events);
+        
+        res.render('events', { events: events });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error fetching events.");
+    }
+});
 
 app.get('/home', async (req, res) => {
     if (req.query.code) {
         // ... (existing token exchange code)
         // Create an authentication provider
-        ensureScope('user.read');
-        const authProvider = {
-            getAccessToken: async () => {
-                // Call getToken in auth.js
-                return await getToken();
-            }
-        };
+        ensureScope('user.read');      
 
         // After acquiring the token, use it to fetch user details:
         // Initialize Graph client with a function as the authProvider
@@ -158,13 +202,14 @@ app.get('/home', async (req, res) => {
 app.get("/redirect", async (req, res) => {
     const tokenRequest = {
         code: req.query.code,
-        scopes: ["user.read"],
+        scopes: ["user.read","Calendars.Read", "Calendars.ReadWrite", "Calendars.ReadBasic", "Calendars.Read.Shared"],
         redirectUri: process.env.REDIRECT_URI
     };
 
     try {
-        const authResponse = await cca.acquireTokenByCode(tokenRequest);    
+        const authResponse = await cca.acquireTokenByCode(tokenRequest);   
         req.session.msalAccount = authResponse.account; // Adjust this according to actual response structure
+        console.log(authResponse);
 
         //res.status(200).send(authResponse.accessToken);
         res.redirect(`/home/?code=${authResponse.accessToken}`);
