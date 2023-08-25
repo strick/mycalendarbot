16187fb3c1efaefc80f34ba1c5fa4e8971d0090a
Brian Strickland <bstrick@gmail.com>
2023-08-25 12:56:32 -0400

    Added a view for permissions

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,21 +1,35 @@
 // controllers/indexController.js
 
 const { initGraphClient } = require('../utils/graphClientHelper');
+const { getToken } = require('../utils/msalHelper'); // Assuming you have a helper to get token
+const jwt = require('jsonwebtoken');
+
 
 exports.getIndex = async (req, res) => {
 
     if (req.session.accessToken) {
-        const client = initGraphClient(req);
-
+        
         try {
+
+            const accessToken = await getToken(req, ["User.Read"]);
+            const client = initGraphClient(req, accessToken);
+
+            // Decode the JWT token
+            const decodedToken = jwt.decode(req.session.accessToken);
+            const grantedScopes = decodedToken.scp.split(' ');
+       
             const userDetails = await client.api('/me').select('id,displayName').get();
+
+            console.log(grantedScopes);
+
             req.session.username = userDetails.displayName;
-            res.render('index', { user: userDetails });
+            res.render('index', { user: userDetails, grantedPermissions: grantedScopes });
+
         } catch (error) {
             console.error(error);
             res.status(500).send(error);
         }
     } else {
-        res.render('index', {user: {}});
+        res.render('index', {user: null, grantedPermissions: []});
     }
 };
