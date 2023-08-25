a80ba3b6d7033c01734440a630a3bf416902e2e0
Brian Strickland <bstrick@gmail.com>
2023-08-25 16:54:26 -0400

    add loading message

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -1,31 +1,33 @@
-const { initGraphClient } = require('../utils/graphClientHelper');
-const { getDateTimeRange, getAccountId, fetchAllEvents, fetchChatSummary } = require('../utils/eventHelpers');
-const handleGraphError = require('../utils/graphErrorHandler');
+// Import necessary utilities and helper functions
+const { initGraphClient } = require('../utils/graphClientHelper'); // To initialize a client connection to Microsoft Graph
+const { getDateTimeRange, getAccountId, fetchAllEvents, fetchChatSummary } = require('../utils/eventHelpers'); // To simplify event fetching and manipulation
+const handleGraphError = require('../utils/graphErrorHandler'); // For handling errors from Microsoft Graph in a standardized way
 
 exports.getEvents = async (req, res) => {
+    // Initialize a new graph client connection using the request's session information
     const client = initGraphClient(req);
+
+    // Fetch the date range that we are interested in (usually for querying events within a specific timeframe)
     const { startDateTime, endDateTime } = getDateTimeRange();
+
+    // Fetch the account ID associated with the current request/session
     const accountId = getAccountId(req);
 
     try {
+        // Fetch all events for the specified account and date range
         const allEvents = await fetchAllEvents(client, accountId, startDateTime, endDateTime);
+
+        // Create a summary of the fetched events for chat display
         const chatSummary = await fetchChatSummary(allEvents);
 
+        // Render the events page with the fetched events and chat summary
         res.render('events', { events: allEvents, chatSummary });
     } catch (error) {
+        // Handle any Microsoft Graph specific errors
         handleGraphError(error);
-
-        try {
-            const client = initGraphClient(req, process.env.VAILD_TOKEN);
-            const allEvents = await fetchAllEvents(client, accountId, startDateTime, endDateTime);
-            const chatSummary = await fetchChatSummary(allEvents, req.session.username);
-
-            res.render('events', { events: allEvents, chatSummary });
-        }
-        catch(error){
-            console.log("error two");
         
-            res.render('events', { events: [] });
-        }
+        // Render the events page without any events (likely due to persistent failures)
+        res.render('events', { events: [] });
+     
     }
 };
